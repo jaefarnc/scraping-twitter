f050a12234522f5ee194548eb6ea8478e7d1ee8e
jaefarnc <130924104+jaefarnc@users.noreply.github.com>
2024-07-11 15:01:54 +0530

    Create dextssh.sh

---
+++ dextssh.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+if tmux has-session -t "0" 2>/dev/null; then
+    session=0
+else 
+    tmux new-session -d -s "0"
+fi
+tmux send-keys -t 0 "/home/student/Downloads/B220032CS/extssh.sh" C-m
+tmux attach-session -t 0
+sleep 100000000000

487958e3af1394b57fb35be2f0b48a33e6d66579
jaefarnc <130924104+jaefarnc@users.noreply.github.com>
2024-07-11 15:06:49 +0530

    Update dextssh.sh

--- dextssh.sh
+++ dextssh.sh
@@ -4,6 +4,6 @@ if tmux has-session -t "0" 2>/dev/null; then
 else 
     tmux new-session -d -s "0"
 fi
-tmux send-keys -t 0 "/home/student/Downloads/B220032CS/extssh.sh" C-m
+tmux send-keys -t 0 "/home/student/Downloads/B220032CS/scraping-twitter/extssh.sh" C-m
 tmux attach-session -t 0
 sleep 100000000000
